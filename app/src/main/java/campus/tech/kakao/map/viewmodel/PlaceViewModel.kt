0a9b1626733a1aba0d074753bbda341b7d6c7ab6
jiyeon <chxistinn@gmail.com>
2024-07-08 16:08:44 +0900

    refactor: move ViewModel class to the viewmodel package

--- app/src/main/java/campus/tech/kakao/map/viewmodel/PlaceViewModel.kt
+++ app/src/main/java/campus/tech/kakao/map/viewmodel/PlaceViewModel.kt
@@ -1,8 +1,11 @@
-package campus.tech.kakao.map
+package campus.tech.kakao.map.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import campus.tech.kakao.map.model.HistoryItem
+import campus.tech.kakao.map.model.PlaceItem
+import campus.tech.kakao.map.repository.PlaceRepository
 
 class PlaceViewModel(application: Application) : AndroidViewModel(application) {
     private val repository: PlaceRepository = PlaceRepository(application)
