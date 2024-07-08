e032115b9637e604bfb59479c8e1258dd6c9ca37
jiyeon <chxistinn@gmail.com>
2024-07-08 15:30:11 +0900

    add: TextWatcher 확장 함수 이용

---
+++ app/src/main/java/campus/tech/kakao/map/ExtendedTextWatcher.kt
@@ -0,0 +1,29 @@
+package campus.tech.kakao.map
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+fun EditText.doOnTextChanged(onTextChanged: (text: CharSequence?) -> Unit) {
+    val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            // Do nothing
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            onTextChanged(s)
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            // Do nothing
+        }
+    }
+    addTextChangedListener(textWatcher)
+    tag = textWatcher
+}
+
+fun EditText.clearTextWatcher() {
+    (tag as? TextWatcher)?.let {
+        removeTextChangedListener(it)
+    }
+}
